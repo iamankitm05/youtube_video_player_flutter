e8b776284866b548dcfb0b46a9621618b32c1e24
Ankit kumar <ankit.ojha@forwardcode.in>
2025-07-25 15:54:05 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# youtube_video_player_flutter
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/youtube_video_player_flutter/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.youtube_video_player_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
